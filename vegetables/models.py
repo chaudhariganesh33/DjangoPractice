ab36b7e9c1b18ac471205d39846756cc1eec6b26
ganesh <chaudhari.ganesh33@gmail.com>
2025-09-07 23:04:19 +0530

    added view count in recipes

--- vegetables/models.py
+++ vegetables/models.py
@@ -6,6 +6,7 @@ class Recipes(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     recipe_image = models.ImageField(upload_to='recipes/')
+    view_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.name + " - " + str(self.view_count)
