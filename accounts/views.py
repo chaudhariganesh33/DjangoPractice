94df31e7010195b194ba7dd422aa170b3793c334
ganesh <chaudhari.ganesh33@gmail.com>
2025-09-11 12:06:41 +0530

    added Custom User Model and it manager

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 # Create your views here.
 
+User = get_user_model()
 
 def login_page(request):
     if request.method == 'POST':
